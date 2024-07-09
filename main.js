f698e7aa6304f8c3f4459c293f3a2a47e54c3b88
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-07-09 16:19:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // const API_KEY = ``;
 let news = []; // 밖으로 빼서 전역 변수 처리
 const getLatestNews = async () => {
-  const url = new URL(
-    `https://willowy-profiterole-943840.netlify.app/top-headlines/top-headlines?country=kr&apiKey=${API_KEY}`
-  );
+  // const url = new URL(
+  //   `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+  // );
+  let url = `https://willowy-profiterole-943840.netlify.app/top-headlines`;
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
